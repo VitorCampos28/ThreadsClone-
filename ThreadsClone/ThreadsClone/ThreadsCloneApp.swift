f8122f7532dcc79a033020d0bc80407f7e628fdc
Vitor Campos <vitor.natal@timwe.com>
2023-11-21 14:37:38 -0300

    first commit

---
+++ ThreadsClone/ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Vitor Campos on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
